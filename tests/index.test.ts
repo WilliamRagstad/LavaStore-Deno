2e9b3d6669c23bdccf6d3b2ace91eb2983151648
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-15 15:18:13 +0100

    Create index.test.ts

---
+++ tests/index.test.ts
@@ -0,0 +1,24 @@
+import { assertEquals, assert } from "https://deno.land/std@0.129.0/testing/asserts.ts";
+import { LavaStore } from "../mod.ts";
+
+/**
+ * Test that an instance of the LavaStore class can be created with a unique ID.
+ */
+Deno.test("Test create LavaStore document with ID", () => {
+  const doc = new LavaStore("test-app");
+  assertEquals(doc.id, "test-app");
+});
+
+/**
+ * Test that a LavaStore document can be created with initial data.
+ */
+Deno.test("Test create LavaStore document with initial data", () => {
+  const doc = new LavaStore("test-app", {
+    test1: "test data",
+    test2: 42,
+    test3: true,
+  });
+  assertEquals(doc.Get("test1"), "test data");
+  assertEquals(doc.Get("test2"), 42);
+  assertEquals(doc.Get("test3"), true);
+});
