9a109ce0e5c825c947310ff29b9a02c02e07bda4
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-15 15:04:25 +0100

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,5 @@
+export { LSDocument } from "./src/LSDocument.ts";
+export { LSCollection } from "./src/LSCollection.ts";
+export { LavaStore } from "./src/LavaStore.ts";
+export type { IDictionary } from "./src/types.ts";
+export { HasFields, Is } from "./src/util.ts";
